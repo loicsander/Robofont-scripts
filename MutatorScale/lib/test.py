e6b4186a72b6a9a4394be38350412de378454b75
Loic Sander <boitaboulet@hotmail.com>
2015-03-29 17:55:57 +0200

    .setAnisotropic no longer exists

--- MutatorScale/lib/test.py
+++ MutatorScale/lib/test.py
@@ -8,8 +8,7 @@ scaler = MutatorScaleEngine(fonts)
 scaler.set({
     'scale':(0.85, 0.8)
     })
-scaler.setAnisotropic(True)
-    
+
 f = RFont(showUI=False)
 f.info.familyName = 'Output'
 
@@ -17,5 +16,5 @@ for glyphName in ['A','H','I','O']:
     stemValues = (100, 70)
     g = scaler.getScaledGlyph(glyphName, stemValues)
     f.insertGlyph(g, glyphName)
-    
+
 f.showUI()
